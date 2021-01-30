233bad5f652c4fc14c7e16db65eabe351695f2db
lausiv1024 <kouki7x2dant@gmail.com>
2021-01-31 00:08:03 +0900

    ジャンプブーストのオンオフ切り替えを可能にした(ほぼ自分用)

--- src/main/java/lausiv1024/items/GodAmulet.java
+++ src/main/java/lausiv1024/items/GodAmulet.java
@@ -1,16 +1,22 @@
 package lausiv1024.items;
 
+import java.util.List;
+
 import lausiv1024.AluminumUtility;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 
 public class GodAmulet extends Item {
-	public static final int ITEMCOUNT = 2;
+	public static final int ITEMCOUNT = 3;
 	public GodAmulet() {
 		super();
 		this.setRegistryName(AluminumUtility.MODID, "god_amulet");
@@ -34,9 +40,24 @@ public class GodAmulet extends Item {
 	}
 
 	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		switch (stack.getItemDamage()) {
+		case 0:
+			tooltip.add("JumpBoost:ON");
+			break;
+
+		case 2:
+			tooltip.add("JumpBoost:OFF");
+			break;
+		}
+	}
+
+	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		int meta = stack.getItemDamage();
-		return this.getUnlocalizedName() + "." + meta;
+		if (meta < 2)
+			return this.getUnlocalizedName() + "." + meta;
+		return this.getUnlocalizedName() + ".0";
 	}
 
 	@Override
@@ -46,5 +67,18 @@ public class GodAmulet extends Item {
 				items.add(new ItemStack(this, 1, i));
 			}
 		}
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		switch (playerIn.getHeldItem(handIn).getItemDamage()) {
+		case 0:
+			playerIn.setHeldItem(handIn, new ItemStack(AluminumUtility.GOD_AMULET, 1, 2));
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+		case 2:
+			playerIn.setHeldItem(handIn, new ItemStack(AluminumUtility.GOD_AMULET, 1, 0));
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+		}
+		return super.onItemRightClick(worldIn, playerIn, handIn);
 	}
 }
